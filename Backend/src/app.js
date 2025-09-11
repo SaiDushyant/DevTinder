540e4037c212612b892ea2dac077ada28b412d78
SaiDushyant <saidushyant04@gmail.com>
2025-09-11 14:55:52 +0530

    Exploring get, post, delete

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ name: "sai", city: "cbe" });
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send("From post");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("From delete");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from test");
 });
