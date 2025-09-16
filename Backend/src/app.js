adcbe1acc497975bb70eefa6512aef5e7b6d6d07
SaiDushyant <saidushyant04@gmail.com>
2025-09-16 18:38:28 +0530

    Added Update users and data validations

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,21 +3,77 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
+// Signup
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Sai",
-    lastName: "Dushyant",
-    emailID: "saidushyant04@gmail.com",
-    password: "s1a2i3$$",
-  });
+  // Creating a new instance of the User model
+  const user = new User(req.body);
 
   try {
     await user.save();
-    res.send("User Added Successfully!!");
-  } catch (error) {
-    res.status(400).send("Error adding User : ", error.message);
+    res.send("User added Successfully!!");
+  } catch {
+    res.status(400).send("Error adding user : ", error.message);
   }
 });
+
+// User API - GET /user Get user data
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailID;
+
+  try {
+    const users = await User.find({ emailID: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch {
+    res.status(400).send("Error fetching the user");
+  }
+});
+
+// Update data of user
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "userId",
+      "image",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const isAllowed = Object.keys(data).every((k) => {
+      ALLOWED_UPDATES.includes(k);
+    });
+
+    if (!isAllowed) {
+      throw new Error("Update not allowed ");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Update Failed:" + error);
+  }
+});
+
+// Feed API - GET /feed - Get all the users from the database
+app.get("/feed", (req, res) => {});
 
 connectDB()
   .then(() => {
--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -1,24 +1,60 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailID: {
+      type: String,
+      required: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data not valid");
+        }
+      },
+    },
+    image: {
+      type: String,
+      default:
+        "https://www.cielhr.com/wp-content/uploads/2020/10/dummy-image.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailID: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
