8183ab58e44db7e515556007d586eac50fb3d8a6
SaiDushyant <saidushyant04@gmail.com>
2025-09-29 10:31:01 +0530

    Made the body to fetch the userdata

--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/costants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispute = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispute(addUser(res.data));
+    } catch (error) {
+      if (error.status === 401) navigate("/login");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
-    <>
+    <div>
       <NavBar />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 };
 
