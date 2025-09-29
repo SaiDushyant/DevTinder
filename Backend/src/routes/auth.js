1bf5fc32a3b7cdf5ed1b02e49a9813c3a4ab4e89
SaiDushyant <saidushyant04@gmail.com>
2025-09-29 05:31:39 +0530

    Completed login and made changes to store cookie token after login

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -49,7 +49,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 7 * 3600000),
       });
-      res.send("Login sucessfull!!");
+      res.send(user);
     } else {
       throw new Error("Invalid credentials");
     }
--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailID, setEmailID] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailID,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="flex justify-center my-20">
+      <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-8">
+        <legend className="fieldset-legend text-4xl">Login</legend>
+
+        <label className="label text-xl">Email</label>
+        <input
+          type="email"
+          className="input"
+          placeholder="Email"
+          value={emailID}
+          onChange={(e) => setEmailID(e.target.value)}
+        />
+
+        <label className="label text-xl mt-4">Password</label>
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4 text-xl" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
